10107abe2f36924769988c77d85d58fa620752c4
Sylvain Beucler <beuc@beuc.net>
2014-06-08 16:08:12 +0200

    Refresh copyright notices

--- src/gfx_palette.c
+++ src/gfx_palette.c
@@ -2,7 +2,7 @@
  * Graphics utilities: palettes...
 
  * Copyright (C) 2003  Shawn Betts
- * Copyright (C) 2003, 2004, 2007, 2008, 2009  Sylvain Beucler
+ * Copyright (C) 2003, 2004, 2007, 2008, 2009, 2014  Sylvain Beucler
 
  * This file is part of GNU FreeDink
 
