847f2d711849edcf084e8e80f139a9cbcba5514c
Sylvain Beucler <beuc@beuc.net>
2014-10-15 17:41:56 +0200

    autobuild: freedink-exe-mxe.sh is now automated and tested

--- autobuild/freedink-exe-mxe.sh
+++ autobuild/freedink-exe-mxe.sh
@@ -25,17 +25,23 @@
 ## Use SDL_gfx 2.0.25: git checkout origin/master src/sdl_gfx.mk
 # make -j2 JOBS=$(nproc) sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_ttf libzip gettext check nsis
 
-function copy_extra_files {
-  mkdir zip/
+PACKAGE=freedink
+VERSION=$(make version | tail -1)
 
-  # documentation
-  for i in README.txt TRANSLATIONS.txt; do
-      cp ../$i zip/freedink-$i
-  done
-  for i in AUTHORS COPYING NEWS THANKS TROUBLESHOOTING; do
-      cp ../$i zip/freedink-$i.txt
-  done
-  cat <<EOF > zip/freedink-SOURCE.txt
+make dist
+rm -rf $PACKAGE-$VERSION/
+tar xzf $PACKAGE-$VERSION.tar.gz
+pushd $PACKAGE-$VERSION/
+mkdir zip/
+
+# documentation
+for i in TRANSLATIONS.txt; do
+    cp $i zip/freedink-$i
+done
+for i in AUTHORS COPYING NEWS README THANKS TROUBLESHOOTING; do
+    cp $i zip/freedink-$i.txt
+done
+cat <<EOF > zip/freedink-SOURCE.txt
 The FreeDink source code is available at:
   http://ftp.gnu.org/gnu/freedink/
 
@@ -47,70 +53,53 @@ If you upload a FreeDink .exe on your website, you must also offer the
 corresponding source code for download.
 EOF
 
-  # TODO: gettext translation files:
-  #cp -a destdir/usr/local/lib/locale zip/po
-  # Note: requires making freedink look in './po/'
-  ## translation template:
-  #cp ../po/dfarc.pot zip/po/
-}
+# Include documentation with MS-DOS newlines (if not already)
+sed -i -e 's/\(^\|[^\r]\)$/\1\r/' zip/freedink-*.txt
 
-PACKAGE=freedink
-VERSION=$1
-if [ -z "$VERSION" ]; then
-    VERSION=$(cd /mnt/snapshots/$PACKAGE && ls -d */ | sed 's,/$,,' | sort -n | tail -1)
-fi
 
-PUBDIR=${PUBDIR:=/mnt/snapshots/$PACKAGE/$VERSION}
-PUBDIR_WOE=$PUBDIR/woe
-mkdir -p $PUBDIR_WOE
-
-rm -rf t/
-mkdir t
-pushd t
-tar xzf $PUBDIR/$PACKAGE-$VERSION.tar.gz
-cd $PACKAGE-$VERSION/
-
-# Install extra-files first, to spot issues early (rather than after
-# several minutes of compilation):
-mkdir -p woe/
-pushd woe
-copy_extra_files
-popd
-
-PATH=/usr/src/mxe/usr/bin:$PATH
-# Already done in the MXE wrapper, but needed for other mingw environments:
-#export PKG_CONFIG="i686-pc-mingw32-pkg-config --static"
-
+##
+# Compile!
+##
 # full-static
-# - specify --build or autoconf will not understand we cross-compile
-#   during some tests
-pushd woe/
+PATH=/usr/src/mxe/usr/bin:$PATH
+
+rm -rf cross-woe-32/
+mkdir cross-woe-32/
+pushd cross-woe-32/
 ../configure --host=i686-w64-mingw32.static \
   --enable-static --enable-upx
 make -j $(nproc)
 make install-strip DESTDIR=$(pwd)/destdir
 # move .exe but avoid symlinks
 find destdir/usr/local/bin/ -type f -name "*.exe" | while read file; do
-  cp -a $file $PUBDIR_WOE/$(basename ${file%.exe}-$VERSION.exe)
-  mv $file zip/
+  mv $file ../zip/
 done
 # Resources
 cp -a destdir/usr/local/share/freedink zip/
-cp -a destdir/usr/share/locale zip/freedink/
-rm -f $PUBDIR_WOE/$PACKAGE-$VERSION-bin.zip
-(cd zip/ && zip -r $PUBDIR_WOE/$PACKAGE-$VERSION-bin.zip *)
+cp -a destdir/usr/local/share/locale zip/freedink/
 popd
-
-popd
-#rm -rf t
-
-exit
+#rm -rf cross-woe-32/
 
 # 64-bit support:
 # - upx-ucl >= 3.91 for w64 support (in Debian >= 8 "Jessie")
 # - use a recent mingw-w64, issues with wchar.h on mingw-w64 v2.0 (GCC 4.6)
 # - NSIS universal installer should be possible:
 #   http://bojan-komazec.blogspot.fr/2011/10/nsis-installer-for-64-bit-windows.html
-../configure --host=x86_64-w64-mingw32 \
+rm -rf cross-woe-64/
+mkdir cross-woe-64/
+pushd cross-woe-64/
+../configure --host=x86_64-w64-mingw32.static \
   --enable-static --enable-upx
-# ...
+make -j$(nproc) install-strip DESTDIR=`pwd`/destdir
+# move .exe but avoid symlinks
+find destdir/usr/local/bin/ -type f -name "*.exe" | while read file; do
+  mv $file ../zip/$(basename ${file%.exe}64.exe)
+done
+popd
+#rm -rf cross-woe-64/
+
+(cd zip/ && zip -r ../../$PACKAGE-$VERSION-bin.zip .)
+
+
+popd
+#rm -rf $PACKAGE-$VERSION/
