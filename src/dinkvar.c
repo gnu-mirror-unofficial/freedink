edb0aeef380adc2b2889622b1c778ddcb223ead6
Sylvain Beucler <beuc@beuc.net>
2014-10-17 15:30:53 +0200

    Never recreate an empty hard.dat in the game
    Conflicts:
    	NEWS
    	src/dinkvar.c

--- src/dinkvar.c
+++ src/dinkvar.c
@@ -725,7 +725,10 @@ void save_hard(void)
 
 
 /**
- * Load hard.dat which contains tile hardness information
+ * Load hard.dat which contains tile hardness information.
+ * 
+ * Unlike 1.08, don't reset and save hard.dat during game in case
+ * e.g. it was just being written by an external editor.
  */
 void load_hard(void)
 {
@@ -742,16 +745,13 @@ void load_hard(void)
   /* Try loading the D-Mod hard.dat */
   f = paths_dmodfile_fopen("hard.dat", "rb");
 
-  /* If running the game, fallback to the default hard.dat, but if
-     running the editor, recreate it in all cases. */
-  if (f == NULL && !dinkedit)
+  /* Fallback to the default hard.dat */
+  if (f == NULL)
     f = paths_fallbackfile_fopen("hard.dat", "rb");
 
   if (f == NULL)
     {
-      //make new data file
-      log_error("Did not find existing hard.dat - creating a new one");
-      save_hard();
+      log_error("Did not find existing hard.dat, using empty data.");
       return;
     }
 
