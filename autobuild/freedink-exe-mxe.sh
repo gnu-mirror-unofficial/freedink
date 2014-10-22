833191a6c06817304e899b86d0a6e5290224a51f
Sylvain Beucler <beuc@beuc.net>
2014-10-22 22:40:52 +0200

    Sync 108.4 doc

--- autobuild/freedink-exe-mxe.sh
+++ autobuild/freedink-exe-mxe.sh
@@ -22,7 +22,6 @@
 # cd /usr/src/mxe/  # master branch
 ## Disable MP3 support: src/sdl_mixer.mk: --enable-music-mp3 -> --disable-music-mp3
 ## Use MinGW-w64 in /usr/src/mxe/settings.mk: MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
-## Use SDL_gfx 2.0.25: git checkout origin/master src/sdl_gfx.mk
 # make -j2 JOBS=$(nproc) sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_ttf libzip gettext check nsis
 
 PACKAGE=freedink
