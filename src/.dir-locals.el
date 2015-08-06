6f3be1ffdaa5edc4cb92561bcf7e397346ddd9b9
Sylvain Beucler <beuc@beuc.net>
2015-08-06 12:35:00 +0200

    Add Emacs directory configuration for coding style

---
+++ src/.dir-locals.el
@@ -0,0 +1,7 @@
+(
+ ; Open .h files with c++-mode
+ (c-mode . ((mode . c++)))
+ ; Try out ScummVM code formatting conventions
+ (c++-mode . ((tab-width . 4)
+	      (c-basic-offset . 4)
+	      (indent-tabs-mode . t))))
