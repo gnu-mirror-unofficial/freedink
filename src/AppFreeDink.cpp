0487cfc8562d839b47fce0b7c1b488b2b2eaff5f
Sylvain Beucler <beuc@beuc.net>
2015-08-09 10:47:52 +0200

    AppFreeDink: indent new file

--- src/AppFreeDink.cpp
+++ src/AppFreeDink.cpp
@@ -34,68 +34,58 @@
 
 /* Text choice selection (e.g. "Load game" in the title screen) */
 void freedink_update_mouse_text_choice(int dx, int dy) {
-  play.mouse += dy;
+	play.mouse += dy;
 }
-void freedink_update_mouse(SDL_Event* ev)
-{
-  if (ev->type != SDL_MOUSEMOTION)
-    return;
+void freedink_update_mouse(SDL_Event* ev) {
+	if (ev->type != SDL_MOUSEMOTION)
+		return;
 
-  /* Players controls mouse */
-  if ((mode == 1 || keep_mouse)
-      && (spr[1].active == 1 && spr[1].brain == 13))
-    {
-      spr[1].x = ev->motion.x;
-      spr[1].y = ev->motion.y;
-      
-      /* Clip the cursor to our client area */
-      if (spr[1].x > (640-1)) spr[1].x = 640-1;
-      if (spr[1].y > (480-1)) spr[1].y = 480-1;
-      if (spr[1].x < 0) spr[1].x = 0;
-      if (spr[1].y < 0) spr[1].y = 0;
-    }
-
-  if (talk.active)
-    freedink_update_mouse_text_choice(ev->motion.xrel, ev->motion.yrel);
+	/* Players controls mouse */
+	if ((mode == 1 || keep_mouse)
+		&& (spr[1].active == 1 && spr[1].brain == 13))
+		{
+			spr[1].x = ev->motion.x;
+			spr[1].y = ev->motion.y;
+			
+			/* Clip the cursor to our client area */
+			if (spr[1].x > (640-1)) spr[1].x = 640-1;
+			if (spr[1].y > (480-1)) spr[1].y = 480-1;
+			if (spr[1].x < 0) spr[1].x = 0;
+			if (spr[1].y < 0) spr[1].y = 0;
+		}
+	
+	if (talk.active)
+		freedink_update_mouse_text_choice(ev->motion.xrel, ev->motion.yrel);
 }
 
 /* Global game shortcuts */
 static void freedink_input_global_shortcuts(SDL_Event* ev) {
-  if (ev->type != SDL_KEYDOWN)
-    return;
-  if (ev->key.repeat)
-    return;
-
-  if (ev->key.keysym.scancode == SDL_SCANCODE_RETURN)
-    {
-      gfx_toggle_fullscreen();
-    }
-  else if (ev->key.keysym.sym == SDLK_q)
-    {
-      //shutdown game
-      SDL_Event ev;
-      ev.type = SDL_QUIT;
-      SDL_PushEvent(&ev);
-    }
-  else if (ev->key.keysym.sym == SDLK_d)
-    {
-      /* Debug mode */
-      if (!debug_mode)
-	log_debug_on();
-      else
-	log_debug_off();
-    }
-  else if (ev->key.keysym.sym == SDLK_c)
-    {
-      if (!console_active)
-	dinkc_console_show();
-      else
-	dinkc_console_hide();
-    }
-  else if (ev->key.keysym.sym == SDLK_m)
-    {
-      //shutdown music
-      StopMidi();
+	if (ev->type != SDL_KEYDOWN)
+		return;
+	if (ev->key.repeat)
+		return;
+	
+	if (ev->key.keysym.scancode == SDL_SCANCODE_RETURN) {
+		gfx_toggle_fullscreen();
+	} else if (ev->key.keysym.sym == SDLK_q) {
+		//shutdown game
+		SDL_Event ev;
+		ev.type = SDL_QUIT;
+		SDL_PushEvent(&ev);
+    } else if (ev->key.keysym.sym == SDLK_d) {
+		/* Debug mode */
+		if (!debug_mode)
+			log_debug_on();
+		else
+			log_debug_off();
+    } else if (ev->key.keysym.sym == SDLK_c) {
+		if (!console_active)
+			dinkc_console_show();
+		else
+			dinkc_console_hide();
+    } else if (ev->key.keysym.sym == SDLK_m) {
+		//shutdown music
+		StopMidi();
     }
 }
 
@@ -161,7 +151,7 @@ void AppFreeDink::input(SDL_Event* ev) {
 		// always tell the game when the player releases a key
 		input_update(ev);
 	} else if ((ev->type == SDL_KEYDOWN || ev->type == SDL_KEYUP)
-			   && ev->key.keysym.mod & KMOD_ALT) {
+			   && (ev->key.keysym.mod & KMOD_ALT)) {
 		freedink_input_global_shortcuts(ev);
 	} else if (console_active) {
 		dinkc_console_process_key(ev);
@@ -183,32 +173,31 @@ void AppFreeDink::logic() {
  * applications like the DinkFrontEnd. Also notify whether Dink is
  * running or not.
  */
-void AppFreeDink::writeDinksmallwoodIni(bool playing)
-{
-  if (g_b_no_write_ini)
-    return; //fix problem with NT security if -noini is set
-  /* TODO: saves it in the user home instead. */
-
+void AppFreeDink::writeDinksmallwoodIni(bool playing) {
+	if (g_b_no_write_ini)
+		return; //fix problem with NT security if -noini is set
+	/* TODO: saves it in the user home instead. */
+	
 #if defined _WIN32 || defined __WIN32__ || defined __CYGWIN__
-  char windir[MAX_PATH];
-  char inifile[MAX_PATH];
-  GetWindowsDirectory(windir, MAX_PATH);
-  sprintf(inifile, "%s\\dinksmallwood.ini", windir);
-
-  unlink(inifile);
-
-  FILE* f = fopen(inifile, "w");
-  if (f == NULL) {
-    log_error("Couldn't write dinksmallwood.ini: %s", strerror(errno));
-    return;
-  }
-  fprintf(f, "[Dink Smallwood Directory Information for the CD to read]\n");
-  fprintf(f, "%s", SDL_GetBasePath());
-  fprintf(f, "\n");
-  if (playing)
-    fprintf(f, "TRUE\n");
-  else
-    fprintf(f, "FALSE\n");
-  fclose(f);
+	char windir[MAX_PATH];
+	char inifile[MAX_PATH];
+	GetWindowsDirectory(windir, MAX_PATH);
+	sprintf(inifile, "%s\\dinksmallwood.ini", windir);
+	
+	unlink(inifile);
+	
+	FILE* f = fopen(inifile, "w");
+	if (f == NULL) {
+		log_error("Couldn't write dinksmallwood.ini: %s", strerror(errno));
+		return;
+	}
+	fprintf(f, "[Dink Smallwood Directory Information for the CD to read]\n");
+	fprintf(f, "%s", SDL_GetBasePath());
+	fprintf(f, "\n");
+	if (playing)
+		fprintf(f, "TRUE\n");
+	else
+		fprintf(f, "FALSE\n");
+	fclose(f);
 #endif
 }
