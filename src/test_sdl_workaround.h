a2a6f32b81c09d5898361c6a9499c0057e487f54
Sylvain Beucler <beuc@beuc.net>
2015-08-20 17:00:57 +0200

    android: enable tests

--- src/test_sdl_workaround.h
+++
@@ -1,7 +0,0 @@
-#ifdef _WIN32
-// Work-around main/SDL_main declaration conflict
-// http://sourceforge.net/p/scummvm/bugs/3412/#c975 "TOOLS: MinGW tools building broken"
-#ifdef main
-#undef main
-#endif
-#endif
