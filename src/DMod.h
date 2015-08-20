254f228cb3c7f7b5daffd6bf09079481b09a701a
Sylvain Beucler <beuc@beuc.net>
2015-08-20 23:17:31 +0200

    DMod: fix compilation on android

--- src/DMod.h
+++ src/DMod.h
@@ -1,5 +1,27 @@
-#ifndef DMOD_H_
-#define DMOD_H_
+/**
+ * Game top static structure
+
+ * Copyright (C) 2015  Sylvain Beucler
+
+ * This file is part of GNU FreeDink
+
+ * GNU FreeDink is free software: you can redistribute it and/or
+ * dmodify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+
+ * GNU FreeDink is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public
+ * License along with GNU FreeDink.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DMOD_H
+#define DMOD_H
 
 #include <string>
 using namespace std;
