af92cab7b8cd1b9eb0d15e9a7c10e6b977ceb4a4
Sylvain Beucler <beuc@beuc.net>
2015-08-25 17:11:54 +0200

    Mark missing gfx primitives

--- src/IOGfxSurfaceGL2.cpp
+++ src/IOGfxSurfaceGL2.cpp
@@ -20,18 +20,23 @@ IOGfxSurfaceGL2::~IOGfxSurfaceGL2() {
 
 /* Function specifically made for Dink'C fill_screen() */
 void IOGfxSurfaceGL2::fill_screen(int num, SDL_Color* palette) {
+	// TODO
 }
 
 void IOGfxSurfaceGL2::fillRect(const SDL_Rect *rect, Uint8 r, Uint8 g, Uint8 b) {
+	// TODO
 }
 
 void IOGfxSurfaceGL2::vlineRGB(Sint16 x, Sint16 y1, Sint16 y2, Uint8 r, Uint8 g, Uint8 b) {
+	// TODO
 }
 void IOGfxSurfaceGL2::hlineRGB(Sint16 x1, Sint16 x2, Sint16 y, Uint8 r, Uint8 g, Uint8 b) {
+	// TODO
 }
 
 
 void IOGfxSurfaceGL2::drawBox(rect box, int color) {
+	// TODO
 }
 
 int IOGfxSurfaceGL2::internalBlit(IOGfxSurface* src, const SDL_Rect* srcrect, SDL_Rect* dstrect, bool useColorKey) {
