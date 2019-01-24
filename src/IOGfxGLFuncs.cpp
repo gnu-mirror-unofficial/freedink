9dd26e4608a6f8931cf5286687d3ca29091a5ffe
Sylvain Beucler <beuc@beuc.net>
2019-01-24 17:51:34 +0100

    emscripten: revert GL static loading, just don't query non-existing glReadBuffer

--- src/IOGfxGLFuncs.cpp
+++ src/IOGfxGLFuncs.cpp
@@ -26,78 +26,68 @@
 
 #include "log.h"
 
-#ifdef __EMSCRIPTEN__
-// static access mandatory since ~1.38.21
-// might be fixed directly in the SDL2 port, cf.
-// https://groups.google.com/d/msg/emscripten-discuss/d3damBim3Mw/mrvJdC49FQAJ
-#include <GLES2/gl2.h>
-#define GETPROCADDRESS(x) x
-#else
-#define GETPROCADDRESS(x) SDL_GL_GetProcAddress(#x)
-#endif
-
 IOGfxGLFuncs::IOGfxGLFuncs() {
-	GetError = (GLenum (APIENTRY*)(void))GETPROCADDRESS(glGetError);
-	Enable = (void (APIENTRY*)(GLenum cap))GETPROCADDRESS(glEnable);
-	GetIntegerv = (void (APIENTRY*)(GLenum pname, GLint* params))GETPROCADDRESS(glGetIntegerv);
-	BlendFunc = (void (APIENTRY*)(GLenum sfactor, GLenum dfactor))GETPROCADDRESS(glBlendFunc);
+	GetError = (GLenum (APIENTRY*)(void))SDL_GL_GetProcAddress("glGetError");
+	Enable = (void (APIENTRY*)(GLenum cap))SDL_GL_GetProcAddress("glEnable");
+	GetIntegerv = (void (APIENTRY*)(GLenum pname, GLint* params))SDL_GL_GetProcAddress("glGetIntegerv");
+	BlendFunc = (void (APIENTRY*)(GLenum sfactor, GLenum dfactor))SDL_GL_GetProcAddress("glBlendFunc");
 
-	Clear = (void (APIENTRY*)(GLbitfield))GETPROCADDRESS(glClear);
-	ClearColor = (void (APIENTRY*)(GLclampf, GLclampf, GLclampf, GLclampf))GETPROCADDRESS(glClearColor);
-	Viewport = (void (APIENTRY*)(GLint x, GLint y, GLsizei width, GLsizei height))GETPROCADDRESS(glViewport);
+	Clear = (void (APIENTRY *)(GLbitfield))SDL_GL_GetProcAddress("glClear");
+	ClearColor = (void (APIENTRY *)(GLclampf, GLclampf, GLclampf, GLclampf))SDL_GL_GetProcAddress("glClearColor");
+	Viewport = (void (APIENTRY *)(GLint x, GLint y, GLsizei width, GLsizei height))SDL_GL_GetProcAddress("glViewport");
 
-	ReadPixels = (void (APIENTRY*)(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *pixels))GETPROCADDRESS(glReadPixels);
+	ReadPixels = (void (APIENTRY *)(GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *pixels))SDL_GL_GetProcAddress("glReadPixels");
 
-	GenTextures = (void (APIENTRY*)(GLsizei n, GLuint *textures))GETPROCADDRESS(glGenTextures);
-	BindTexture = (void (APIENTRY*)(GLenum target, GLuint texture))GETPROCADDRESS(glBindTexture);
-	DeleteTextures = (void (APIENTRY*)(GLsizei n, const GLuint *textures))GETPROCADDRESS(glDeleteTextures);
-	TexParameteri = (void (APIENTRY*)(GLenum target, GLenum pname, GLint param))GETPROCADDRESS(glTexParameteri);
-	TexImage2D = (void (APIENTRY*)(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels))GETPROCADDRESS(glTexImage2D);
+	GenTextures = (void (APIENTRY*)(GLsizei n, GLuint *textures))SDL_GL_GetProcAddress("glGenTextures");
+	BindTexture = (void (APIENTRY*)(GLenum target, GLuint texture))SDL_GL_GetProcAddress("glBindTexture");
+	DeleteTextures = (void (APIENTRY*)(GLsizei n, const GLuint *textures))SDL_GL_GetProcAddress("glDeleteTextures");
+	TexParameteri = (void (APIENTRY*)(GLenum target, GLenum pname, GLint param))SDL_GL_GetProcAddress("glTexParameteri");
+	TexImage2D = (void (APIENTRY*)(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels))SDL_GL_GetProcAddress("glTexImage2D");
 
-	GenBuffers = (void (APIENTRY*)(GLsizei n, GLuint* buffers))GETPROCADDRESS(glGenBuffers);
-	BindBuffer = (void (APIENTRY*)(GLenum target, GLuint buffer))GETPROCADDRESS(glBindBuffer);
-	BufferData = (void (APIENTRY*)(GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage))GETPROCADDRESS(glBufferData);
-	DeleteBuffers = (void (APIENTRY*)(GLsizei n, const GLuint* buffers))GETPROCADDRESS(glDeleteBuffers);
+	GenBuffers = (void (APIENTRY*)(GLsizei n, GLuint* buffers))SDL_GL_GetProcAddress("glGenBuffers");
+	BindBuffer = (void (APIENTRY*)(GLenum target, GLuint buffer))SDL_GL_GetProcAddress("glBindBuffer");
+	BufferData = (void (APIENTRY*)(GLenum target, GLsizeiptr size, const GLvoid* data, GLenum usage))SDL_GL_GetProcAddress("glBufferData");
+	DeleteBuffers = (void (APIENTRY*)(GLsizei n, const GLuint* buffers))SDL_GL_GetProcAddress("glDeleteBuffers");
 
-	IsShader = (GLboolean (APIENTRY*)(GLuint shader))GETPROCADDRESS(glIsShader);
-	CreateShader = (GLuint (APIENTRY*)(GLenum type))GETPROCADDRESS(glCreateShader);
-	ShaderSource = (void (APIENTRY*)(GLuint shader, GLsizei count, const GLchar** strings, const GLint* lengths))GETPROCADDRESS(glShaderSource);
-	CompileShader = (void (APIENTRY*)(GLuint shader))GETPROCADDRESS(glCompileShader);
-	GetShaderiv = (void (APIENTRY*)(GLuint shader, GLenum pname, GLint* param))GETPROCADDRESS(glGetShaderiv);
-	GetShaderInfoLog = (void (APIENTRY*)(GLuint shader, GLsizei bufSize, GLsizei* length, GLchar* infoLog))GETPROCADDRESS(glGetShaderInfoLog);
-	AttachShader = (void (APIENTRY*)(GLuint program, GLuint shader))GETPROCADDRESS(glAttachShader);
-	DeleteShader = (void (APIENTRY*)(GLuint shader))GETPROCADDRESS(glDeleteShader);
+	IsShader = (GLboolean (APIENTRY *)(GLuint shader))SDL_GL_GetProcAddress("glIsShader");
+	CreateShader = (GLuint (APIENTRY *)(GLenum type))SDL_GL_GetProcAddress("glCreateShader");
+	ShaderSource = (void (APIENTRY*)(GLuint shader, GLsizei count, const GLchar** strings, const GLint* lengths))SDL_GL_GetProcAddress("glShaderSource");
+	CompileShader = (void (APIENTRY*)(GLuint shader))SDL_GL_GetProcAddress("glCompileShader");
+	GetShaderiv = (void (APIENTRY*)(GLuint shader, GLenum pname, GLint* param))SDL_GL_GetProcAddress("glGetShaderiv");
+	GetShaderInfoLog = (void (APIENTRY*)(GLuint shader, GLsizei bufSize, GLsizei* length, GLchar* infoLog))SDL_GL_GetProcAddress("glGetShaderInfoLog");
+	AttachShader = (void (APIENTRY*)(GLuint program, GLuint shader))SDL_GL_GetProcAddress("glAttachShader");
+	DeleteShader = (void (APIENTRY*)(GLuint shader))SDL_GL_GetProcAddress("glDeleteShader");
 
-	IsProgram = (GLboolean (APIENTRY*)(GLuint program))GETPROCADDRESS(glIsProgram);
-	CreateProgram = (GLuint (APIENTRY*)(void))GETPROCADDRESS(glCreateProgram);
-	GetProgramiv = (void (APIENTRY*)(GLuint program, GLenum pname, GLint* param))GETPROCADDRESS(glGetProgramiv);
-	LinkProgram = (void (APIENTRY*)(GLuint program))GETPROCADDRESS(glLinkProgram);
-	GetProgramInfoLog = (void (APIENTRY*)(GLuint program, GLsizei bufSize, GLsizei* length, GLchar* infoLog))GETPROCADDRESS(glGetProgramInfoLog);
-	DeleteProgram = (void (APIENTRY*)(GLuint program))GETPROCADDRESS(glDeleteProgram);
+	IsProgram = (GLboolean (APIENTRY *)(GLuint program))SDL_GL_GetProcAddress("glIsProgram");
+	CreateProgram = (GLuint (APIENTRY *)(void))SDL_GL_GetProcAddress("glCreateProgram");
+	GetProgramiv = (void (APIENTRY*)(GLuint program, GLenum pname, GLint* param))SDL_GL_GetProcAddress("glGetProgramiv");
+	LinkProgram = (void (APIENTRY*)(GLuint program))SDL_GL_GetProcAddress("glLinkProgram");
+	GetProgramInfoLog = (void (APIENTRY*)(GLuint program, GLsizei bufSize, GLsizei* length, GLchar* infoLog))SDL_GL_GetProcAddress("glGetProgramInfoLog");
+	DeleteProgram = (void (APIENTRY*)(GLuint program))SDL_GL_GetProcAddress("glDeleteProgram");
 
-	GetAttribLocation = (GLint (APIENTRY*)(GLuint program, const GLchar* name))GETPROCADDRESS(glGetAttribLocation);
-	GetUniformLocation = (GLint (APIENTRY*)(GLuint program, const GLchar* name))GETPROCADDRESS(glGetUniformLocation);
+	GetAttribLocation = (GLint (APIENTRY*)(GLuint program, const GLchar* name))SDL_GL_GetProcAddress("glGetAttribLocation");
+	GetUniformLocation = (GLint (APIENTRY*)(GLuint program, const GLchar* name))SDL_GL_GetProcAddress("glGetUniformLocation");
 
-	UseProgram = (void (APIENTRY*)(GLuint program))GETPROCADDRESS(glUseProgram);
-	ActiveTexture = (void (APIENTRY*)(GLenum texture))GETPROCADDRESS(glActiveTexture);
-	Uniform1i = (void (APIENTRY*)(GLint location, GLint v0))GETPROCADDRESS(glUniform1i);
-	Uniform1f = (void (APIENTRY*)(GLint location, GLfloat v0))GETPROCADDRESS(glUniform1f);
-	Uniform3f = (void (APIENTRY*)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2))GETPROCADDRESS(glUniform3f);
-	Uniform4f = (void (APIENTRY*)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3))GETPROCADDRESS(glUniform4f);
-	UniformMatrix3fv = (void (APIENTRY*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value))GETPROCADDRESS(glUniformMatrix3fv);
-	UniformMatrix4fv = (void (APIENTRY*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value))GETPROCADDRESS(glUniformMatrix4fv);
+	UseProgram = (void (APIENTRY*)(GLuint program))SDL_GL_GetProcAddress("glUseProgram");
+	ActiveTexture = (void (APIENTRY*)(GLenum texture))SDL_GL_GetProcAddress("glActiveTexture");
+	Uniform1i = (void (APIENTRY*)(GLint location, GLint v0))SDL_GL_GetProcAddress("glUniform1i");
+	Uniform1f = (void (APIENTRY*)(GLint location, GLfloat v0))SDL_GL_GetProcAddress("glUniform1f");
+	Uniform3f = (void (APIENTRY*)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2))SDL_GL_GetProcAddress("glUniform3f");
+	Uniform4f = (void (APIENTRY*)(GLint location, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3))SDL_GL_GetProcAddress("glUniform4f");
+	UniformMatrix3fv = (void (APIENTRY*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value))SDL_GL_GetProcAddress("glUniformMatrix3fv");
+	UniformMatrix4fv = (void (APIENTRY*)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* value))SDL_GL_GetProcAddress("glUniformMatrix4fv");
 
-	EnableVertexAttribArray = (void (APIENTRY*)(GLuint))GETPROCADDRESS(glEnableVertexAttribArray);
-	VertexAttribPointer = (void (APIENTRY*)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid* pointer))GETPROCADDRESS(glVertexAttribPointer);
-	DisableVertexAttribArray = (void (APIENTRY*)(GLuint))GETPROCADDRESS(glDisableVertexAttribArray);
+	EnableVertexAttribArray = (void (APIENTRY*)(GLuint))SDL_GL_GetProcAddress("glEnableVertexAttribArray");
+	VertexAttribPointer = (void (APIENTRY*)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const GLvoid* pointer))SDL_GL_GetProcAddress("glVertexAttribPointer");
+	DisableVertexAttribArray = (void (APIENTRY*)(GLuint))SDL_GL_GetProcAddress("glDisableVertexAttribArray");
 
-	GenFramebuffers = (void (APIENTRY*)(GLsizei n, GLuint* framebuffers))GETPROCADDRESS(glGenFramebuffers);
-	BindFramebuffer = (void (APIENTRY*)(GLenum target, GLuint framebuffer))GETPROCADDRESS(glBindFramebuffer);
-	FramebufferTexture2D = (void (APIENTRY*)(GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level))GETPROCADDRESS(glFramebufferTexture2D);
-	CheckFramebufferStatus = (GLenum (APIENTRY*)(GLenum target))GETPROCADDRESS(glCheckFramebufferStatus);
-	DeleteFramebuffers = (void (APIENTRY*)(GLsizei n, const GLuint* framebuffers))GETPROCADDRESS(glDeleteFramebuffers);
+	GenFramebuffers = (void (APIENTRY*)(GLsizei n, GLuint* framebuffers))SDL_GL_GetProcAddress("glGenFramebuffers");
+	BindFramebuffer = (void (APIENTRY*)(GLenum target, GLuint framebuffer))SDL_GL_GetProcAddress("glBindFramebuffer");
+	FramebufferTexture2D = (void (APIENTRY*)(GLenum target, GLenum attachment, GLenum textarget, GLuint texture, GLint level))SDL_GL_GetProcAddress("glFramebufferTexture2D");
+	CheckFramebufferStatus = (GLenum (APIENTRY*)(GLenum target))SDL_GL_GetProcAddress("glCheckFramebufferStatus");
+	DeleteFramebuffers = (void (APIENTRY*)(GLsizei n, const GLuint* framebuffers))SDL_GL_GetProcAddress("glDeleteFramebuffers");
 
-	DrawArrays = (void (APIENTRY*)(GLenum mode, GLint first, GLsizei count))GETPROCADDRESS(glDrawArrays);
+	DrawArrays = (void (APIENTRY*)(GLenum mode, GLint first, GLsizei count))SDL_GL_GetProcAddress("glDrawArrays");
 }
 
 void IOGfxGLFuncs::logGetError() {
